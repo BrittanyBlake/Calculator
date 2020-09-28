88371d3249564523bbf30fec165a78d82c44d105
Brittany Blake <brittany.blake49@gmail.com>
2020-09-29 01:11:08 +0200

    add App component

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ButtonPanel from './ButtonPanel'
+import Display from './Display'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Calculator</h1>
+      <ButtonPanel />
+      <Display />
+    </div>
+  );
+}
+
+export default App;
