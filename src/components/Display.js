18db0a02502104b4f8b0cba1be3bc8046a8b12f5
Brittany Blake <brittany.blake49@gmail.com>
2020-09-29 01:21:20 +0200

    implement display component

---
+++ src/components/Display.js
@@ -0,0 +1,19 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function Display({ result }) {
+  return (
+    <div>
+      <h3> {result} </h3>
+    </div>
+  );
+}
+
+Display.PropTypes = {
+  result: PropTypes.string,
+};
+Display.defaultProps = {
+  result: '0',
+};
+
+export default Display;
