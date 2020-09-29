795b1d601dcf420a439b2f1a1ae3c8d91eca91d5
Brittany Blake <brittany.blake49@gmail.com>
2020-09-29 17:45:32 +0200

    turn app.js to a class component

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,20 +1,32 @@
-import React from 'react';
+import React, { Component } from "react";
 import Display from '../display/Display';
 import ButtonPanel from '../button-panel/ButtonPanel';
 // eslint-disable-next-line
 import calculate from '../../logic/calculate';
 import styles from './App.module.css';
 
-function App() {
-  return (
-    <div className={styles.container}>
-      <div className={styles.App}>
-        <h1 className={styles.title}>Calculator</h1>
-        <Display />
-        <ButtonPanel />
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      total: null,
+      next: null,
+      operation: null,
+    };
+  }
+
+  render() {
+    return (
+      <div className={styles.container}>
+        <div className={styles.App}>
+          <h1 className={styles.title}>Calculator</h1>
+          <Display />
+          <ButtonPanel />
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
