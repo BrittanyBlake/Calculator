88b16487989742753ecf58d452de8d445b30e573
Brittany Blake <brittany.blake49@gmail.com>
2020-09-29 16:24:06 +0200

    add operate methods

---
+++ src/logic/operate.js
@@ -0,0 +1,21 @@
+import Big from 'big.js'
+
+const operate = (numberOne, numberTwo, operation) => {
+
+    const num1 = Big(numberOne)
+    const num2 = Big(numberTwo)
+    let total = 0
+
+    if (operation == "-"){
+        total = num1.minus(num2)
+    } else if (operation == "+"){
+        total = num1.plus(num2)
+    } else if (operation == 'X'){
+        total = num1.times(num2)
+    } else if (operation == "÷") {
+        total = num1.div(num2)
+    } else if (operation == "%"){
+        total = num1.mod(num2)
+    }
+    return total;
+}
