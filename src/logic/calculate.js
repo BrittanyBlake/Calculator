03a432306bfb69a10e8b22ae85e699fe370c0ed3
Brittany Blake <brittany.blake49@gmail.com>
2020-09-30 15:45:21 +0200

    fixing big number error

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -9,20 +9,23 @@ const calculate = (dataObject, buttonName) => {
     next = null;
     operation = null;
   } else if (buttonName === '+/-') {
-    total *= -1;
-    next *= -1;
+    next = (next * -1).toString();
+    total =(total * -1).toString();
+    
+    // total *= -1;
+    // next *= -1;
   } else if (buttonName === '.' && next) {
     if (!next.includes('.')) {
       next += '.';
     }
   } else if (buttonName === '=') {
-    if (next && total) {
-      total = operate(total, next, operation);
+    if (next && total && operation) {
+      total = operate(total, next, operation).toString();
       next = null;
       operation = null;
     }
   } else if (operators.includes(buttonName)) {
-    total = operate(total, next, operation);
+    total = operate(total, next, operation).toString();
   }
 
   return { total, next, operation };
--- src/logic/operate.js
+++ src/logic/operate.js
@@ -3,27 +3,27 @@ import Big from 'big.js';
 const operate = (numberOne, numberTwo, operation) => {
   const num1 = Big(numberOne);
   const num2 = Big(numberTwo);
-  let total = 0;
+  let total;
 
   switch (operation) {
     case '-':
-      total = num1.minus(num2);
-      break;
+      total = num1.minus(num2).toString();
+      break
     case '+':
-      total = num1.plus(num2);
+      total = num1.plus(num2).toString();
       break;
     case 'X':
-      total = num1.times(num2);
+      total = num1.times(num2).toString();
       break;
     case '÷':
       if (num1 === 0 || num2 === 0) {
         total = 'NaN';
       } else {
-        total = num1.div(num2);
+        total = num1.div(num2).toString();
       }
       break;
     case '%':
-      total = num1.times('0.01');
+      total = num1.times("0.01").toString();
       break;
     default:
   }
